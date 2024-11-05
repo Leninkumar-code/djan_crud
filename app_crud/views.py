9dda95ceff52650a13b6c3bab4b8a833a613b0ee
Leninkumar-code <sleninkumar9@gmail.com>
2024-11-05 06:57:26 +0530

    Corrections

--- app_crud/views.py
+++ app_crud/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from app_crud.models import student
 from app_crud.forms import StudentForm
+from django.urls import reverse
 # Create your views here.
 
 
@@ -16,14 +17,14 @@ def create (b):
         forms = StudentForm(b.POST)
         if forms.is_valid():
             forms.save()
-            return redirect('display')
+            return redirect(reverse('display'))
     return render (b,'create.html',{'forms':form})
 
 # -----------It will delete the whole row ---------------------------------->
 def delete_(req,id):
     det=student.objects.get(id=id)
     det.delete()
-    return redirect('display')
+    return redirect(reverse('display'))
 
 # -----------It can update/or edit the datas in a row ----------------------->
 def update_(req,id):
@@ -33,7 +34,7 @@ def update_(req,id):
         u=StudentForm(req.POST,instance=upd)
         if u.is_valid():
             u.save()
-            return redirect('display')
+            return redirect(reverse('display'))
     return render(req,'create.html',{'forms':frm_obj})
     
     
--- proj_crud/urls.py
+++ proj_crud/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app_crud import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('',views.display,name='display'),
     path('create/',views.create),
     path('del/<id>',views.delete_),
